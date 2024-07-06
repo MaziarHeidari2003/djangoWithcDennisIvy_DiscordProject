64dca2a93ba103b04dc73d08b95091e169a8ec3d
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-07-06 17:51:28 +0430

    lets move to custon user model

--- studybud/settings.py
+++ studybud/settings.py
@@ -136,3 +136,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORES_ALLOW_ORIGINES= True
